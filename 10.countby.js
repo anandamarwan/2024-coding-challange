16fbc3c9804f2640f17f4ca0e46c1d7b59eea9bd
anandamarwan <anandapuspita2@gmail.com>
2024-06-16 16:10:23 +0700

    feat: create question 10

---
+++ 10.countby.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
+
+function countBy(x, n) {
+  let z = [];
+
+  for (let i = 1; i <= n; i++) {
+    z.push(x * i);
+  }
+
+  return z;
+}
+console.log(countBy(3, 3));
